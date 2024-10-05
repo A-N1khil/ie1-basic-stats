fbe4291576616e333e94b593921bb90c0fda2af6
A-N1khil <nikhilanand1006@gmail.com>
2024-10-05 19:59:46 -0400

    MainFunction

--- src/MinMaxCalculator.java
+++ src/MinMaxCalculator.java
@@ -1,2 +1,8 @@
 public class MinMaxCalculator {
+
+    public static void main(String[] args) {
+        
+        // Create an array of double numbers
+        double numbers[] = { 1.9, 6.7, 8.9, 7.8, 2.0 };
+    }
 }
