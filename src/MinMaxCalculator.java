cb9dfa5e94d4786f13a4ae20209e5b02739b5557
Rakshita-7 <rakshitasriv@umass.edu>
2024-10-05 20:11:23 -0400

    Adding function to calculate min

--- src/MinMaxCalculator.java
+++ src/MinMaxCalculator.java
@@ -5,4 +5,20 @@ public class MinMaxCalculator {
         // Create an array of double numbers
         double numbers[] = { 1.9, 6.7, 8.9, 7.8, 2.0 };
     }
+
+    public static double min(double[] num)
+    {
+        if(num.length == 0 ){
+            return 0;
+        }
+        double min = num[0];
+        for (int i=0; i < num.length; i++)
+        {
+            if(num[i]<min){
+                min=num[i];
+            }
+
+        }
+        return min;
+    }
 }
